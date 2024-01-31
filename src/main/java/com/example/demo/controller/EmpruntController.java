304c8fdf2a079365c438ab853fbb505bd5261649
Boryana Baeva <b.baeva@labom2iformation.fr>
2024-01-31 19:10:13 +0100

    REST API : added methods POST and GET ALL for Emprunt (SO issue to be resolved with DTO)

--- src/main/java/com/example/demo/controller/EmpruntController.java
+++ src/main/java/com/example/demo/controller/EmpruntController.java
@@ -1,9 +1,63 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Document;
+import com.example.demo.model.Emprunt;
+import com.example.demo.service.MediathequeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api")
 public class EmpruntController {
+    @Autowired
+    private MediathequeService mediathequeService;
+
+    @GetMapping("emprunts")
+    public List<Emprunt> getAll() {
+        return mediathequeService.getAllEmprunts();
+    }
+
+    @PostMapping("emprunts")
+    public ResponseEntity<?> save(@RequestBody Emprunt emprunt) {
+        List<String> errors = getEmpruntPostErrors(emprunt);
+        if(!errors.isEmpty()) {
+            return ResponseEntity.badRequest().body(errors.toString());
+        }
+        else {
+            mediathequeService.saveEmprunt(emprunt);
+            return ResponseEntity.ok(emprunt);
+        }
+    }
+
+    private List<String> getEmpruntPostErrors(Emprunt emprunt) {
+        List<String> errors = new ArrayList<>();
+
+        if(emprunt.getAdherent() == null ) {
+            errors.add("Adherent obligatoire !");
+        }
+
+        if(emprunt.getDocument() == null ) {
+            errors.add("Document obligatoire !");
+        }
+
+        if(!emprunt.getDocument().getIsAvailable()) {
+            errors.add("Ce document n'est pas disponible !");
+        }
+
+        if(mediathequeService.getAllDocuments().stream()
+                .noneMatch(d -> emprunt.getDocument().getId().equals(d.getId()))) {
+            errors.add("Document avec id "+ emprunt.getDocument().getId() + " inexistant !");
+        }
+
+        if(mediathequeService.getAllAdherents().stream()
+                .noneMatch(a -> emprunt.getAdherent().getId().equals(a.getId()))) {
+            errors.add("Adherent avec id "+ emprunt.getDocument().getId() + " inexistant !");
+        }
+
+        return errors;
+    }
 }
